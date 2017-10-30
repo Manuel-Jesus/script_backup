0899b9b45d6884215f64d7c8ca7078cc0b5b3ba1
Manuel-Jesus <manueljesusbeginesgonzalez@gmail.com>
2017-10-30 19:15:51 +0100

    adaptado el script para que sshfs

--- script_backup.py
+++ script_backup.py
@@ -18,9 +18,14 @@ destino_dir=settings["destino_dir"]
 sql_usuario=settings["sql_usuario"]
 sql_pass=settings["sql_pass"]
 hostname=settings["hostname"]
+local_load_folder=settings["local_load_folder"]
 nombreArchivo=hostname+"_"+fecha
-direccionArchivo=directorioTemporal+nombreArchivo
+direccionArchivo=local_load_folder+nombreArchivo
 
+
+#montando directorio remoto
+
+os.system("sshfs "+destino_user+"@"+destino_host+":"+destino_dir+" "+local_load_folder+" -o IdentiyFile=~/.ssh/"+hostname)
 
 
 #exportacion de la base de datos
@@ -35,8 +40,12 @@ for carpeta in directorios:
     os.system("zip -r "+direccionArchivo+".zip" + " "+carpeta)
 
 #Copiamos archivo a la maquina de destino con SCP
-os.system("scp -i ~/.ssh/"+hostname+" "+direccionArchivo+".zip "+destino_user+"@"+destino_host+":"+destino_dir)
+#os.system("scp -i ~/.ssh/"+hostname+" "+direccionArchivo+".zip "+destino_user+"@"+destino_host+":"+destino_dir)
 
 #borramos el zip creado
 
-os.system("rm "+direccionArchivo+".zip")
+##os.system("rm "+direccionArchivo+".zip")
+
+#Desmontamos el directorio remoto
+
+os.system("umount "+local_load_folder)
